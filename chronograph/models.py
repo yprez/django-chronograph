d8b4b8670dad73987a640d872f33068e1a4bd48b
Steve Pulec <spulec@gmail.com>
2011-06-28 15:13:16 -0700

    Fixed bug with setting next run.

--- chronograph/models.py
+++ chronograph/models.py
@@ -166,8 +166,8 @@ class Job(models.Model):
         self.save()
         
         if save:
-            self.last_run = run_date
-            self.next_run = self.rrule.after(run_date)
+            self.last_run = self.next_run
+            self.next_run = self.rrule.after(self.next_run)
             self.save()
         
         stdout_str += stdout.getvalue()
